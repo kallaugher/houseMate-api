28467d107a54842894cddd0ff8592b810edb4d74
kallaugher <alicekallaugher@gmail.com>
2016-10-03 12:20:57 -0400

    pass first user test

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,33 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160930193656) do
+ActiveRecord::Schema.define(version: 20161003155155) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "assignments", force: :cascade do |t|
+    t.integer  "user_id"
+    t.integer  "event_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "events", force: :cascade do |t|
+    t.string   "name"
+    t.string   "type"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "groups", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
+    t.string   "name"
     t.string   "email"
     t.string   "password_digest"
     t.datetime "created_at",      null: false
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,12 +1,10 @@
 require 'rails_helper'
 
-let(:alice) {User.new(name: "Alice", email: "alice@gmail.com", password: "password")}
-
 RSpec.describe User, type: :model do
-  it 'should be valid with a name, email and password'
+
+  let(:alice) {User.new(name: "Alice", email: "alice@gmail.com", password: "password")}
+
+  it 'should be valid with a name, email and password' do
     expect(alice).to be_valid
   end
 end
-
-
-
