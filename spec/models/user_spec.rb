b9d9dd61946b6382821e92399a57cf4e02166ab5
kallaugher <alicekallaugher@gmail.com>
2016-10-03 12:23:04 -0400

    add invalid user tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -7,4 +7,19 @@ RSpec.describe User, type: :model do
   it 'should be valid with a name, email and password' do
     expect(alice).to be_valid
   end
+
+  it 'should be invalid without a name' do
+    let(:gentian) {User.new(email: "gentian@gmail.com", password: "password")}
+    expect(gentian).to_not be_valid
+  end
+
+  it 'should be invalid without an email' do
+    let(:gentian) {User.new(name: "Gentian", password: "password")}
+    expect(gentian).to_not be_valid
+  end
+
+  it 'should be invalid without a password' do
+    let(:gentian) {User.new(name: "Gentian", email: "gentian@gmail.com")}
+    expect(gentian).to_not be_valid
+  end
 end
