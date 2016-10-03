5c61f06f49a769c2a5209dbfda16c9a6745d5aaf
jasmin47 <alxsanborn@gmail.com>
2016-10-03 13:30:31 -0400

    add tests for full name; validate name, email, password

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,24 +2,34 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
 
-  let(:alice) {User.new(name: "Alice", email: "alice@gmail.com", password: "password")}
+  let(:alice) {User.new(first_name: "Alice", last_name: "Kallaugher", email: "alice@gmail.com", password: "password")}
 
   it 'should be valid with a name, email and password' do
     expect(alice).to be_valid
   end
 
-  it 'should be invalid without a name' do
-    let(:gentian) {User.new(email: "gentian@gmail.com", password: "password")}
+  it 'should be invalid without a first name' do
+    gentian = User.new(last_name: "bardhoshi", email: "gentian@gmail.com", password: "password")
+    expect(gentian).to_not be_valid
+  end
+  it 'should be invalid without a last name' do
+    gentian = User.new(first_name: "gentian", email: "gentian@gmail.com", password: "password")
     expect(gentian).to_not be_valid
   end
 
   it 'should be invalid without an email' do
-    let(:gentian) {User.new(name: "Gentian", password: "password")}
+    gentian = User.new(first_name: "Gentian", last_name: "Bardhoshi", password: "password")
     expect(gentian).to_not be_valid
   end
 
   it 'should be invalid without a password' do
-    let(:gentian) {User.new(name: "Gentian", email: "gentian@gmail.com")}
+    gentian = User.new(first_name: "Gentian", last_name: "Bardhoshi", email: "gentian@gmail.com")
     expect(gentian).to_not be_valid
   end
+
+  it 'should have a full name' do
+    gentian = User.new(first_name: "Gentian", last_name: "Bardhoshi", email: "gentian@gmail.com", password: "password")
+    expect(gentian.full_name).to eq("Gentian Bardhoshi")
+  end
+
 end
