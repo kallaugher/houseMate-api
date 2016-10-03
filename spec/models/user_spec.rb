c9d91646e38fe6a46e065fd48952404aa2d787a8
tnegoshi <gbardhoshi@gmail.com>
2016-10-03 12:13:39 -0400

    add user model testing

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
+let(:alice) {User.new(name: "Alice", email: "alice@gmail.com", password: "password")}
+
 RSpec.describe User, type: :model do
-  # pending "add some examples to (or delete) #{__FILE__}"
+  it 'should be valid with a name, email and password'
+    expect(alice).to be_valid
+  end
 end
+
+
+
