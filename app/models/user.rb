ca952cd286de26971c0b4c898b4f83f04cc87c2a
tnegoshi <gbardhoshi@gmail.com>
2016-10-03 14:02:06 -0400

    user testing helper methods + associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,12 @@
 class User < ApplicationRecord
   has_secure_password
-  validates_presence_of :name, :email, :password
+  validates_presence_of :first_name, :last_name, :email, :password
+
+  def full_name
+    (self.first_name + ' ' + self.last_name).chomp.strip
+  end
+
+  def get_initials
+    (self.first_name[0] + self.last_name[0]).upcase
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,35 +1,58 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-
   let(:alice) {User.new(first_name: "Alice", last_name: "Kallaugher", email: "alice@gmail.com", password: "password")}
+  let(:gentian) {User.new(first_name: "Gentian", last_name: "Bardhoshi", email: "gentian@gmail.com", password: "password", group_id: 1, event_id: 1)}
+  let(:aga) {Group.new(id: 1 , name: "A-Good-Abode")}
+  let(:house_party) {Event.new(id: 1, name: "Dodgeball Mayhem 2")}
+  let(:clean_bathroom) {Assignment.new(user_id: 1, event_id: 1, name: "clean bathroom")}
+
 
   it 'should be valid with a name, email and password' do
     expect(alice).to be_valid
   end
 
   it 'should be invalid without a first name' do
-    gentian = User.new(last_name: "bardhoshi", email: "gentian@gmail.com", password: "password")
-    expect(gentian).to_not be_valid
+    invalid_gentian = User.new(last_name: "bardhoshi", email: "gentian@gmail.com", password: "password")
+    expect(invalid_gentian).to_not be_valid
   end
   it 'should be invalid without a last name' do
-    gentian = User.new(first_name: "gentian", email: "gentian@gmail.com", password: "password")
-    expect(gentian).to_not be_valid
+    invalid_gentian = User.new(first_name: "gentian", email: "gentian@gmail.com", password: "password")
+    expect(invalid_gentian).to_not be_valid
   end
 
   it 'should be invalid without an email' do
-    gentian = User.new(first_name: "Gentian", last_name: "Bardhoshi", password: "password")
-    expect(gentian).to_not be_valid
+    invalid_gentian = User.new(first_name: "Gentian", last_name: "Bardhoshi", password: "password")
+    expect(invalid_gentian).to_not be_valid
   end
 
   it 'should be invalid without a password' do
-    gentian = User.new(first_name: "Gentian", last_name: "Bardhoshi", email: "gentian@gmail.com")
-    expect(gentian).to_not be_valid
+    invalid_gentian = User.new(first_name: "Gentian", last_name: "Bardhoshi", email: "gentian@gmail.com")
+    expect(invalid_gentian).to_not be_valid
   end
 
   it 'should have a full name' do
-    gentian = User.new(first_name: "Gentian", last_name: "Bardhoshi", email: "gentian@gmail.com", password: "password")
     expect(gentian.full_name).to eq("Gentian Bardhoshi")
+  end
+
+  it 'should respond with intials to #get_initials' do
+    expect(gentian.get_initials).to eq("GB")
+  end
+
+  it 'should belong to a group' do
+    expect(!!gentian.group).to eq("true")
+    expect(gentian.group).to eq(aga)
+  end
+
+  it 'should have many events through groups' do
+    expect(gentian.events).to be_a_kind_of(Array)
+    expect(gentian.events).to eq(aga.events)
+    expect(gentian.events).to incude(house_party)
+  end
+
+  it 'should have assignments through events' do
+    expect(gentian.assignments).to be_a_kind_of(Array)
+    expect(gentian.assignments).to eq(house_party.assignments)
   end
 
 end
