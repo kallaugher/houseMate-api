7148f497608f6e66805b27231bebc494fe73d749
jasmin47 <alxsanborn@gmail.com>
2016-10-03 20:57:07 -0400

    add groups spec tests

--- spec/models/group_spec.rb
+++ spec/models/group_spec.rb
@@ -1,5 +1,35 @@
 require 'rails_helper'
 
 RSpec.describe Group, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:headbanders){Group.create(name:"The Headband House")}
+  let(:alex){User.create(first_name: "Alex", last_name: "Sanborn", email: "alex@gmail.com", password: "password")}
+  let(:movie_night) {Event.create(name:"Movie night")}
+
+  context 'Group validations' do
+    it 'should be valid with a name' do
+      expect(headbanders).to be_valid
+    end
+
+    it 'should be invalid without a name' do
+      no_name = Group.new()
+      expect(no_name).to_not be_valid
+    end
+
+    it 'should be invalid with blank spaces' do
+      blank_spaces = Group.new(name: "   ")
+      expect(blank_spaces).to_not be_valid
+    end
+  end
+
+  context 'Group relationships' do
+    it 'is associated with a user' do
+      headbanders.users << alex
+      expect(headbanders.users).to include(alex)
+    end
+
+    it 'is associated with an event' do
+      headbanders.events << movie_night
+      expect(headbanders.events).to include(movie_night)
+    end
+  end
 end
