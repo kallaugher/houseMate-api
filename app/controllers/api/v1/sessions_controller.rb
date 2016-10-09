d7ed0b6fb155769ca49a960ca58ccacb16f587f2
kallaugher <alicekallaugher@gmail.com>
2016-10-08 21:44:05 -0400

    fix auth params

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -5,14 +5,14 @@ module Api
 
       def create
         # we had to change the line below, as there was no id in the params
-        user = User.find_by(email: params[:email])
-        if user.authenticate(params[:password])
+        user = User.find_by(email: auth_params[:email])
+        if user.authenticate(auth_params[:password])
           jwt = Auth.issue({user: user.id})
           render json: {jwt: jwt}
         else
           # render json: {"Unable to authenticate email and password, please try again."}
           # find out how json error messages are stored and test specifically for new error instead of default
-          render json:
+          # render json:
         end
       end
 
